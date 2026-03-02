bd5f809a36978c4f3d7be834c8be1f8b7e3b13e8
lperezCES <lperez@ces.com.uy>
2026-03-02 17:03:05 -0300

    Pruebas de Wikipedia

---
+++ wikipedia-test-e2e/android/busqueda/data/busqueda.data.js
@@ -0,0 +1,9 @@
+output.busquedaData = {
+  "termino_busqueda": "Hello World",
+
+  validaciones: {
+    "texto_resultado": ', a computer program that outputs or displays the message "Hello, World!"',
+    "texto_saludo": "Hello,orld!"
+  }
+
+}
---
+++ wikipedia-test-e2e/android/busqueda/pom/navegacion.pom.js
@@ -0,0 +1,8 @@
+
+output.navegacionPom = {
+  "explorar": "nav_tab_explore",
+  "guardado": "nav_tab_reading_lists",
+  "buscar": "nav_tab_search",
+  "editar": "nav_tab_edits",
+  "mas": "nav_more_container"
+}
