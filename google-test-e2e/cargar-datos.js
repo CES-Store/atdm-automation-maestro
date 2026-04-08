16df76e5d3526cf2e68ecaf0606bcaa084c8f59e
lperezCES <lperez@ces.com.uy>
2026-04-08 15:36:39 -0300

    Estrategia para incluir un juego de datos

---
+++ google-test-e2e/cargar-datos.js
@@ -0,0 +1,5 @@
+output.googleSearch = [
+  { terminoBusqueda: "https://maestro.dev/", resultadoEsperado: "End-to-end testing for Mobile and Web apps." },
+  { terminoBusqueda: "https://flutter.dev/", resultadoEsperado: "Build for any screen" },
+  { terminoBusqueda: "https://capacitacion.ces.com.uy/", resultadoEsperado: "CES - AULA VIRTUAL" }
+];
